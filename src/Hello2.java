316f6fea3587c8075624c2844e3de067c5612c9d
dlwogmlzz <lwwljh90@gmail.com>
2026-08-07 11:22:21 +0900

    20260807 Java Study # Day0 - IntelliJ단축키 정리

---
+++ src/Hello2.java
@@ -0,0 +1,9 @@
+public class Hello2 {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        System.out.println("c = " + c);
+        System.out.println("b = " + b);
+    }
+}
