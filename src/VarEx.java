1da32fe62d1dee73a9eeafed8eee36fdf41c8835
dlwogmlzz <lwwljh90@gmail.com>
2026-08-07 12:48:12 +0900

    20260807 Java Study # Day1 - 변수

---
+++ src/VarEx.java
@@ -0,0 +1,13 @@
+public class VarEx {
+    public static void main(String[] args) {
+        int age; // 1. 변수 선언 - 저장 공간 만들기
+        int z;
+
+        age = 10; // 2. 쓰기 - 변수에 값 저장하기
+        z = 20;
+        System.out.println(age); // 3. 읽기 - 변수에 저장된 값 읽어오기
+        age = age + 10;
+        System.out.println("age = " + age); // soutv
+        System.out.println("z = " + z); // z.soutv
+    }
+}
---
+++ src/VarEx2.java
@@ -0,0 +1,16 @@
+public class VarEx2 {
+    // x, y 값 변경하기.
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+        int tmp; // 빈 컵 역할
+
+        tmp = x;
+        x = y;
+        y = tmp;
+
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+        System.out.println("tmp = " + tmp);
+    }
+}
