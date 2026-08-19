863dc90c836bb3cdff90454586e056fc2edb50f7
dlwogmlzz <lwwljh90@gmail.com>
2026-08-19 17:28:48 +0900

    20260819 Java Silver - 6장 인스턴스&메서드 17번 문제 코드 확인, 코멘트 수정

--- src/silver/e6n17.java
+++ src/silver/e6n17.java
@@ -5,7 +5,7 @@ public class e6n17 {
         e6n17 m = new e6n17();
         System.out.println(m.calc(2,3));
     }
-    // index와 호출할때의 타입이 맞아야 됨. 아니면 에러발생
+    // index와 출력할때의 값(int면 int로)이 맞아야 됨. 아니면 에러발생
     private double calc(int a, int b) {
         return (a+b) / 2;
     }
